6fea1ad1a38f6ef9cf3691342f9473b8ef3c68e8
bo-domi <86868222+bo-domi@users.noreply.github.com>
2021-10-02 10:31:09 +0900

    条件分岐〜record.rb追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,8 @@
 class ItemsController < ApplicationController
-  
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:edit, :show, :update, :destroy]
+  before_action :move_to_index, only: [:update, :edit, :destroy]
+
 def index
   @items = Item.order("id DESC")
 end
@@ -17,9 +20,48 @@ def create
   end
 end
 
+def destroy
+  if @item.destroy
+    redirect_to action: "index"
+   else
+    render :show
+   end
+end
+
+def edit
+  if @item.record.present?
+    redirect_to root_path
+   end
+end
+
+def show
+    
+end
+
+def update
+  if @item.update(item_params)
+   redirect_to action: "show"
+  else
+   render :edit
+  end
+end
+
+
 private
+
   def item_params
     params.require(:item).permit(:title, :text, :category_id, :detail_id, :delivery_price_id, :day_id, :price, :area_id, images: []).merge(user_id: current_user.id)
   end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    if current_user.id != @item.user_id
+      redirect_to root_path
+    end
+  end
+
 
 end
