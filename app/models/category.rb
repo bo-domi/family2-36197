43b95224ff74db8bcc3c030a29ff0f0f39d24485
bo-domi <86868222+bo-domi@users.noreply.github.com>
2021-10-06 15:06:07 +0900

    カテゴリー変更

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,16 +1,21 @@
 class Category < ActiveHash::Base
   self.data = [
     { id: 1, name: '--' },
-    { id: 2, name: 'メンズ' },
-    { id: 3, name: 'レディース' },
-    { id: 4, name: 'ベビー・キッズ' },
-    { id: 5, name: 'インテリア・住まい・小物' },
-    { id: 6, name: '本・音楽・ゲーム' },
-    { id: 7, name: 'おもちゃ・ホビー・グッズ' },
-    { id: 8, name: '家電・スマホ・カメラ' },
-    { id: 9, name: 'スポーツ・レジャー' },
-    { id: 10, name: 'ハンドメイド' },
-    { id: 11, name: 'その他' }
+    { id: 2, name: 'ベビー服（女の子用）~95cm' },
+    { id: 3, name: 'ベビー服（男の子用）~95cm' },
+    { id: 4, name: 'ベビー服（男女兼用）~95cm' },
+    { id: 5, name: 'キッズ服（女の子用）100cm~' },
+    { id: 6, name: 'キッズ服（男の子用）100cm~' },
+    { id: 7, name: 'キッズ服（男女兼用）100cm~' },
+    { id: 8, name: 'キッズ靴' },
+    { id: 9, name: '子供用ファッション小物' },
+    { id: 10, name: 'おむつ/トイレ/バス' },
+    { id: 11, name: '授乳/食事' },
+    { id: 12, name: 'ベビー家具/室内用品' },
+    { id: 13, name: '学習用品' },
+    { id: 14, name: '行事/記念品' },
+    { id: 15, name: 'その他' }
+
   ]
 
   include ActiveHash::Associations
